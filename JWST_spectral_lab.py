93790216cd86b483cf3746ea60e2567e2171971e
Jan Scholtz <honzascholtz@gmail.com>
2025-09-14 16:46:51 +0100

    redshift update

--- JWST_spectral_lab.py
+++ JWST_spectral_lab.py
@@ -1,4 +1,4 @@
 # Importing the class 
 import Spectral_lab_core as SED
 # Running the class to initialize the SED fitter 
-SED.JWST_Spectral_lab()
+SED.JWST_Spectral_lab_redshift()
--- JWST_spectral_lab_redshift.py
+++ JWST_spectral_lab_redshift.py
@@ -7,6 +7,7 @@ Created on Sun April 27 21:23:34 2025
 """
 
 #importing modules            
+import sys
 from astropy.io import fits as pyfits
 import Graph_setup as gst
 import numpy as np
@@ -26,13 +27,12 @@ from matplotlib.widgets import Slider, Button, RadioButtons, RangeSlider, TextBo
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
-import bagpipes as pipes
-
-import sys
-sys.setrecursionlimit(1500)
+    
 pth = sys.path[0]
 
-class JWST_Spectral_lab:
+
+
+class JWST_Spectral_lab_redshift:
     '''Class to visualize SED'''
     def __init__(self, ):
         """
@@ -50,7 +50,6 @@ class JWST_Spectral_lab:
             3, 3, height_ratios=(2.2,0.7,1.3), width_ratios=(1,1,1))
         
         self.ax0 = self.fig.add_subplot(gs[0,:])
-        self.axres = self.fig.add_subplot(gs[1,:], sharex=self.ax0)
 
         plt.subplots_adjust(hspace=0)
 
@@ -60,15 +59,8 @@ class JWST_Spectral_lab:
             self.data_error = hdu['ERR'].data*1e-7
             
         self.ax0.plot(self.data_wave, self.data_flux, color='black', drawstyle='steps-mid')
-        self.z = 9.431
-        self.Mass = 9.
-        self.age = 0.3
-        self.tau = 0.3
-        self.Z = 1,
-        self.U = -3
-        self.Av = 0.5
-
-        self.pregenerate_model()
+        self.z = 1
+        
         self.target = 'generic'
         
 
@@ -80,57 +72,12 @@ class JWST_Spectral_lab:
         x0 = 0.1
 
         # Redshift slider
-        #self.slider_z_ax = plt.axes([x0, y0, 0.8, 0.03])
-        #self.z_slider = Slider(self.slider_z_ax, 'redshift', valmin=1,
-        #                      valmax=15,
-        #                      valinit=self.z)
-        #self.z_slider.on_changed(self.slide_update)
+        self.slider_z_ax = plt.axes([x0, y0, 0.8, 0.03])
+        self.z_slider = Slider(self.slider_z_ax, 'redshift', valmin=1,
+                              valmax=15,
+                              valinit=self.z)
+        self.z_slider.on_changed(self.slide_update)
 
-        # Mass Slider
-        self.slider_mass_ax = plt.axes([x0, y0-dy, 0.8, 0.03])
-        self.mass_slider = Slider(self.slider_mass_ax, 'Mass', valmin=7,
-                                   valmax=12,
-                                    valinit=self.Mass)
-        self.mass_slider.on_changed(self.slide_update)
-
-        # Log U slider
-        self.slider_logU_ax = plt.axes([x0, y0-2*dy, 0.8, 0.03])
-        self.logU_slider = Slider(self.slider_logU_ax, 'Radiation\nstrength', valmin=-4.01,
-                                   valmax=-1,
-                                    valinit=-2)
-        self.logU_slider.on_changed(self.slide_update)
-
-        # Metal Slider
-        self.slider_metal_ax = plt.axes([x0, y0-3*dy, 0.8, 0.03])
-        self.metal_slider = Slider(self.slider_metal_ax, 'Heavy\nelements', valmin=0.01,
-                                   valmax=1.4,
-                                    valinit=0.5)
-        self.metal_slider.on_changed(self.slide_update)
-    
-        # Age Slider
-        self.slider_age_ax = plt.axes([x0, y0-4*dy, 0.8, 0.03])
-        self.age_slider = Slider(self.slider_age_ax, 'Age of Stars', valmin=-2,
-                                 valmax=1,
-                                 valinit=-1)
-        self.age_slider.on_changed(self.slide_update)
-
-        # Age Slider
-        self.slider_tau_ax = plt.axes([x0, y0-5*dy, 0.8, 0.03])
-        self.tau_slider = Slider(self.slider_tau_ax, 'Decline of stars', valmin=-2,
-                                 valmax=1,
-                                 valinit=-1)
-        self.tau_slider.on_changed(self.slide_update)
-
-        # Dust Slider
-        self.slider_dust_ax = plt.axes([x0, y0-6*dy, 0.8, 0.03])
-        self.dust_slider = Slider(self.slider_dust_ax, 'Dust', valmin=0.,
-                                 valmax=3,
-                                 valinit=0.2)
-        self.dust_slider.on_changed(self.slide_update)
-        # Text box for redshift input
-        #axbox = plt.axes([0.95, y0, 0.03, 0.03])
-        #text_box = TextBox(axbox, '', initial='')
-        #text_box.on_submit(self.submit_redshift)
 
         # Button to load SF943 data
         axbutton1 = plt.axes([0.1,0.9, 0.05,0.05])
@@ -162,7 +109,6 @@ class JWST_Spectral_lab:
         self.SF2 = Button(axbutton6, 'SF2', color='lightblue', hovercolor='lightgreen')
         self.SF2.on_clicked(self.SF2_galaxy_load)
 
-        self.generate_model()
         self.plot_general()
         plt.show()
 
@@ -178,8 +124,7 @@ class JWST_Spectral_lab:
             self.data_error = hdu['ERR'].data*1e-7
 
             self.target = 'generic'
-            self.z = 9.436
-
+            
             self.plot_general()
         
     def QC_galaxy_load(self, event):
@@ -187,8 +132,6 @@ class JWST_Spectral_lab:
             self.data_wave = hdu['WAVELENGTH'].data*1e6
             self.data_flux = hdu['DATA'].data*1e-7
             self.data_error = hdu['ERR'].data*1e-7
-
-            self.z = 2.820
 
             self.target = 'generic'
 
@@ -200,8 +143,6 @@ class JWST_Spectral_lab:
             self.data_flux = hdu['DATA'].data*1e-7
             self.data_error = hdu['ERR'].data*1e-7
 
-            self.z = 5.4431
-
             self.target = 'generic'
 
             self.plot_general()
@@ -211,8 +152,6 @@ class JWST_Spectral_lab:
             self.data_wave = hdu['WAVELENGTH'].data*1e6
             self.data_flux = hdu['DATA'].data*1e-7
             self.data_error = hdu['ERR'].data*1e-7
-
-            self.z = 3.6591
 
             self.target = 'generic'
 
@@ -224,8 +163,6 @@ class JWST_Spectral_lab:
             self.data_flux = hdu['DATA'].data*1e-7
             self.data_error = hdu['ERR'].data*1e-7
 
-            self.z = 14.18
-
             self.target = 'GSz14'
 
             self.plot_general()
@@ -236,8 +173,6 @@ class JWST_Spectral_lab:
             self.data_flux = hdu['DATA'].data*1e-7
             self.data_error = hdu['ERR'].data*1e-7
 
-            self.z = 6.856
-
             self.data_wave = np.append(self.data_wave, np.linspace(5.32,5.5, 32))
             self.data_flux = np.append(self.data_flux, np.zeros(32))
             self.data_error = np.append(self.data_error, np.ones(32)*0.001e-18)
@@ -247,117 +182,35 @@ class JWST_Spectral_lab:
             self.target = 'generic'
 
     def slide_update(self,val):
-        #self.z = self.z_slider.val
-        self.Mass = self.mass_slider.val
-        self.U = self.logU_slider.val
-        self.Z = self.metal_slider.val
-        self.age = 10**self.age_slider.val
-        self.tau = 10**self.tau_slider.val
-        self.Av = self.dust_slider.val
-        self.generate_model()
+        self.z = self.z_slider.val
+        
         self.plot_general()
     
-    def pregenerate_model(self, ):
-        """
-        Pre-generate the model components to speed up the generation of the model
-        """
-        exponential = {}  # Tau model star formation history component
-        exponential["age"] = self.age  # Gyr
-        exponential["tau"] = self.tau  # Gyr
-        exponential["massformed"] = self.Mass  # log_10(M*/M_solar)
-        exponential["metallicity"] = self.Z  # Z/Z_oldsolar
-
-        dust = {}  # Dust component
-        dust["type"] = "Calzetti"  # Define the shape of the attenuation curve
-        dust["Av"] = self.Av  # magnitudes
-
-        model_components = {}  # The model components dictionary
-        if self.U>-4:
-            nebular = {}  # Nebular emission component
-            nebular["logU"] = self.U  # log_10(ionization parameter)
-            model_components["nebular"] = nebular
-        
-        
-        model_components["redshift"] = self.z  # Observed redshift
-        model_components["exponential"] = exponential
-        model_components["dust"] = dust
-        with pyfits.open(pth + "/Data/jwst_nirspec_prism_disp.fits") as hdul:
-            model_components["R_curve"] = np.c_[1e4*hdul[1].data["WAVELENGTH"], hdul[1].data["R"]]
-
-        self.model = pipes.model_galaxy(
-            model_components, spec_wavs=self.data_wave*1e4)
-
-    def generate_model(self, ):
-        
-        exponential = {}  # Tau model star formation history component
-        exponential["age"] = self.age  # Gyr
-        exponential["tau"] = self.tau  # Gyr
-        exponential["massformed"] = self.Mass  # log_10(M*/M_solar)
-        exponential["metallicity"] = self.Z  # Z/Z_oldsolar
-
-        dust = {}  # Dust component
-        dust["type"] = "Calzetti"  # Define the shape of the attenuation curve
-        dust["Av"] = self.Av  # magnitudes
-
-        model_components = {}  # The model components dictionary
-        if self.U>-5:
-            nebular = {}  # Nebular emission component
-            nebular["logU"] = self.U  # log_10(ionization parameter)
-            model_components["nebular"] = nebular
-        
-        
-        model_components["redshift"] = self.z  # Observed redshift
-        model_components["exponential"] = exponential
-        model_components["dust"] = dust
-        with pyfits.open(pth+"/Data/jwst_nirspec_prism_disp.fits") as hdul:
-            model_components["R_curve"] = np.c_[1e4*hdul[1].data["WAVELENGTH"], hdul[1].data["R"]]
-
-
-        self.model.update(model_components)
 
        
     def plot_general(self, event=None):
         self.ax0.clear()       
-        self.axres.clear()
-
-        if np.nansum(self.model.spectrum[:, 1]/1e-18) ==0:
-            self.ax0.plot(self.data_wave, self.data_flux/1e-18, color='black', drawstyle='steps-mid')
-
-            self.axres.plot(self.data_wave, (self.data_flux-self.model.spectrum[:, 1])/self.data_error, color='black', drawstyle='steps-mid')
-            self.axres.axhline(0, color='red', lw=1.5, ls='--')
-
-            self.ax0.set_ylabel("Flux [10$^{-18}$ erg/s/cm$^2$/Å]")
-            self.axres.set_xlabel("Wavelength [microns]")
-            self.ax0.set_xlim(0.5, 5.3)
-
-            self.ax0.text(0.05, 0.75,'Your stars are older than the Universe, reduce Age parameter!',\
-                        transform = self.ax0.transAxes)
-
-        else:
-            self.ax0.plot(self.model.spectrum[:, 0]/1e4, self.model.spectrum[:, 1]/1e-18, color='firebrick', drawstyle='steps-mid')
-            self.ax0.fill_between(self.data_wave,  (self.data_flux-self.data_error)/1e-18,  (self.data_flux+self.data_error)/1e-18, \
+        
+        self.ax0.plot(self.data_wave, self.data_flux/1e-18, color='black', drawstyle='steps-mid')
+        self.ax0.fill_between(self.data_wave,  (self.data_flux-self.data_error)/1e-18,  (self.data_flux+self.data_error)/1e-18, \
                                   color='lightgrey', alpha=0.5, step='mid')
 
-            self.ax0.plot(self.data_wave, self.data_flux/1e-18, color='black', drawstyle='steps-mid')
+        self.ax0.plot(self.data_wave, self.data_flux/1e-18, color='black', drawstyle='steps-mid')
 
-            self.axres.plot(self.data_wave, (self.data_flux-self.model.spectrum[:, 1])/self.data_error, color='black', drawstyle='steps-mid')
-            self.axres.fill_between(self.data_wave, -self.data_error/1e-18, self.data_error/1e-18, color='lightgrey', alpha=0.5, step='mid')
-            self.axres.axhline(0, color='red', lw=1.5, ls='--')
+        self.ax0.set_ylabel("Flux [10$^{-18}$ erg/s/cm$^2$/Å]")
+            
+        self.ax0.set_xlim(0.5, 5.3)
 
-            self.ax0.set_ylabel("Flux [10$^{-18}$ erg/s/cm$^2$/Å]")
-            self.axres.set_xlabel("Wavelength [microns]")
-            self.axres.set_ylabel("Residuals [sigma]")
-            self.ax0.set_xlim(0.5, 5.3)
+        #self.score = np.nansum((self.data_flux- self.model.spectrum[:, 1])**2/self.data_error**2)/(len(self.data_flux)-6)
 
-            self.score = np.nansum((self.data_flux- self.model.spectrum[:, 1])**2/self.data_error**2)/(len(self.data_flux)-6)
+        #self.ax0.text(0.05, 0.75,'(Smaller is better)\nscore: {:.2f}'.format(self.score),\
+        #                transform = self.ax0.transAxes, bbox=dict(boxstyle='Round,pad=0.01', facecolor='white',
+        #                    alpha=1.0, edgecolor='none'))
 
-            self.ax0.text(0.05, 0.75,'(Smaller is better)\nscore: {:.2f}'.format(self.score),\
-                        transform = self.ax0.transAxes, bbox=dict(boxstyle='Round,pad=0.01', facecolor='white',
-                            alpha=1.0, edgecolor='none'))
-
-            self.labels_eml()
+        self.labels_eml()
         if self.target == 'GSz14':
             self.ax0.set_ylim(-0.00025, 0.01)
+        
         self.fig.canvas.draw()
   
 
@@ -420,6 +273,3 @@ class JWST_Spectral_lab:
                             alpha=1.0, edgecolor='none'),
                 zorder=99,
                 )        
-
-
-
